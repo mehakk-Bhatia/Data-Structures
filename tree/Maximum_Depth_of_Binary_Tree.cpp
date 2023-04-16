36ad5ab144e64a338b0e4bdd392a889af399aa1a
mehakk-Bhatia <91878394+mehakk-Bhatia@users.noreply.github.com>
2023-04-16 20:23:09 +0530

    Update Maximum_Depth_of_Binary_Tree.cpp

--- tree/Maximum_Depth_of_Binary_Tree.cpp
+++ tree/Maximum_Depth_of_Binary_Tree.cpp
@@ -11,19 +11,12 @@
  */
 class Solution {
 public:
-    void f(TreeNode* root , vector<int> &v){
-         if(root == NULL){
-             return ;
-         }
-         f(root->left , v);
-         v.push_back(root->val);
-         f(root->right , v);
-    }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> v;
-        
-        f(root , v);
-        return v;
-        
+    int maxDepth(TreeNode* root) {
+        if (root == NULL){
+            return 0;
+        }
+        else{
+            return max(maxDepth(root->left) , maxDepth(root->right)) + 1;
+        }
     }
 };
