0c44cb9eb3b73251170b2b055ed633cab47dd389
mehakk-Bhatia <91878394+mehakk-Bhatia@users.noreply.github.com>
2023-06-13 21:44:30 +0530

    leetcode #2733 weekly contest 349

---
+++ arrays/Neither Minimum nor Maximum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1 || n==2){
+            return -1;
+        }
+        sort(nums.begin() , nums.end());
+        return nums[1];
+        
+    }
+};
